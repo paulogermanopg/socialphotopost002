8338e0e68183804cbf60774842b4904c37a754f6
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-06-13 15:04:57 -0300

    login e register

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { createBottomTabNavigator } from 'react-navigation'
+import { createBottomTabNavigator, createSwitchNavigator, createStackNavigator } from 'react-navigation'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
+import Login from './screens/Login'
+import Register from './screens/Register'
+
+const authRouter = createStackNavigator({
+    Login: { screen: Login, navigationOptions: { title: 'Login' } },
+    Register: { screen: Register, navigationOptions: { title: 'Register' } }
+}, {
+    initialRouteName: 'Login'
+})
+
+const loginOrprofileRouter = createSwitchNavigator({
+    Profile: Profile,
+       Auth: authRouter
+    }, {
+        initialRouteNam: 'Profile'
+    }
+)
 
 const MenuRoutes = {
     Feed: {
@@ -27,7 +44,7 @@ const MenuRoutes = {
     },
     Profile: {
         name: 'Profile',
-        screen: Profile,
+        screen: loginOrprofileRouter,
         navigationOptions: {
             title: 'Profile',
             tabBarIcon: ({ tintColor }) =>
