787f5e9129f8458a0ce15c334ce26e29e7c7f7da
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-06-18 13:51:43 -0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Gravatar } from 'react-native-gravatar'
 import { View, StyleSheet, Text, Platform, Image } from 'react-native'
 import icon from '../../assets/imgs/icon.png'
 
 class Header extends Component {
     render() {
+        const name = this.props.name || 'Anônimo'
+        const gravatar = this.props.email ? 
+            <Gravatar options={{ email: this.props.email, secure: true }}
+                style={styles.avatar} />
+            : null
         return(
             <View style={styles.container}>
                 <View style={styles.rowContainer}>
                     <Image source={icon} style={styles.image} />
                     <Text style={styles.title}>SocialPost</Text>
                 </View>
+                <View style={styles.userContainer}>
+                    <Text style={styles.user}>{name}</Text>
+                    {gravatar}
+                </View>
             </View>
+            
         )
     } 
 }
@@ -22,6 +34,8 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         borderColor: '#BBB',
         width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
     rowContainer: {
         flexDirection: 'row',
@@ -38,7 +52,29 @@ const styles = StyleSheet.create({
         fontFamily: 'TravelingTypewriter',
         height: 30,
         fontSize: 28
+    },
+    userContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    user: {
+        fontSize: 10,
+        color: '#888'
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        marginLeft: 10,
     }
 })
 
-export default Header
+const mapStateToProps = ({ user }) => {
+    return {
+        email: user.email,
+        name: user.name,
+    }
+}
+
+export default connect(mapStateToProps)(Header)
+
+//export default Header
