59b7242b17a77426c62cce08d782786ff9d4d285
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-07-09 14:51:42 -0300

    splashscream

--- src/Navigator.js
+++ src/Navigator.js
@@ -7,6 +7,7 @@ import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
 import Login from './screens/Login'
 import Register from './screens/Register'
+import Splash from './screens/Splash'
 
 const authRouter = createStackNavigator({
     Login: { screen: Login, navigationOptions: { title: 'Login' } },
@@ -61,4 +62,12 @@ const MenuConfig = {
 }
 
 const MenuNavigator = createBottomTabNavigator(MenuRoutes, MenuConfig)
-export default MenuNavigator
+
+const SplashRouter = createSwitchNavigator({
+    Splash: Splash,
+    App: MenuNavigator,
+}, {
+    initialRouteName: 'Splash'
+})
+
+export default SplashRouter
